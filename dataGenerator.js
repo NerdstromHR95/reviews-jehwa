58c2de60ce2ff63588174969504b2802981b4626
Jehwa Shin <s.jehwa@gmail.com>
2018-05-31 19:59:19 -0700

    Added eslint

--- dataGenerator.js
+++ dataGenerator.js
@@ -1,23 +1,24 @@
 const mongoose = require('mongoose');
 const data = require('./mockData.js');
+
 mongoose.connect('mongodb://localhost/reviews');
 
-let reviewSchema = mongoose.Schema({
-    id: {type: Number, unique: true},
-    itemNo: Number,
-    nickname: String,
-    title: String,
-    description: String,
-    stars: Number,
-    fitRating: Number,
-    widthRating: Number    
-})
+const reviewSchema = mongoose.Schema({
+  id: { type: Number, unique: true },
+  itemNo: Number,
+  nickname: String,
+  title: String,
+  description: String,
+  stars: Number,
+  fitRating: Number,
+  widthRating: Number,
+});
 
-let Review = mongoose.model('Review', reviewSchema);
+const Review = mongoose.model('Review', reviewSchema);
 
 Review.insertMany(data, (err) => {
-    if(err) {
-        console.log(err, 'error during building mock data');
-    } 
-    mongoose.connection.close();
-})
+  if (err) {
+    console.log(err, 'error during building mock data');
+  }
+  mongoose.connection.close();
+});
