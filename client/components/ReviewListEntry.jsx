edf0f647bab86179d649a1815fcae87406ec89f1
Jehwa Shin <s.jehwa@gmail.com>
2018-06-04 15:02:03 -0700

    refactored rightColumn components

--- client/components/ReviewListEntry.jsx
+++ client/components/ReviewListEntry.jsx
@@ -1,160 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import FitRating from './FitRating.jsx';
+import WidthRating from './WidthRating.jsx';
 
-const ReviewListEntry = (props) => {
-  const fitRating = {
-    0: '',
-    1: 'Small',
-    2: 'Slightly small',
-    3: 'True to size',
-    4: 'Slightly large',
-    5: 'Large', 
-  };
-  const widthRating = {
-    0: '',
-    1: 'Narrow',
-    2: 'Slightly narrow',
-    3: 'True to width',
-    4: 'Slightly wide',
-    5: 'Wide', 
-  };
-  if(props.review.fitRating && props.review.widthRating) {
-    return (
-      <div className="review">
-        <div className="leftColumn">
-          <div className="title">
-            {props.review.title}
-          </div>
-          <div className="description">
-            {props.review.description}
-          </div>
-          <div className="reviewerInfo">
-            <span>
-              {props.review.nickname}
-            </span>
-            <span>
-              Sweepstakes entry
-            </span>
-          </div>
-        </div>
-        <div className="rightColumn">
-          <span>
-            {new Date(props.review.date).toLocaleString('en-US', { year: 'numeric', day: 'numeric', month: 'long' })}
-          </span>
-          <div>
-            <strong>
-              Fit:
-            </strong>
-            <span>
-              {` ${fitRating[props.review.fitRating]}`}
-            </span>
-            <br />
-            <strong>
-              Width:
-            </strong>
-            <span>
-              {` ${widthRating[props.review.widthRating]}`}
-            </span>
-          </div>
-        </div>
-      </div>
-    );
-  }
-  if(props.review.fitRating) {
-    return(
-      <div className="review">
-        <div className="leftColumn">
-          <div className="title">
-            {props.review.title}
-          </div>
-          <div className="description">
-            {props.review.description}
-          </div>
-          <div className="reviewerInfo">
-            <span>
-              {props.review.nickname}
-            </span>
-            <span>
-              Sweepstakes entry
-            </span>
-          </div>
-        </div>
-        <div className="rightColumn">
-          <span>
-            {new Date(props.review.date).toLocaleString('en-US', { year: 'numeric', day: 'numeric', month: 'long' })}
-          </span>
-          <div>
-            <strong>
-              Fit:
-            </strong>
-            <span>
-              {` ${fitRating[props.review.fitRating]}`}
-            </span>
-          </div>
-        </div>
-      </div>
-    );
-  }
-  if(props.review.widthRating) {
-    return(
-      <div className="review">
-        <div className="leftColumn">
-          <div className="title">
-            {props.review.title}
-          </div>
-          <div className="description">
-            {props.review.description}
-          </div>
-          <div className="reviewerInfo">
-            <span>
-              {props.review.nickname}
-            </span>
-            <span>
-              Sweepstakes entry
-            </span>
-          </div>
-        </div>
-        <div className="rightColumn">
-          <span>
-            {new Date(props.review.date).toLocaleString('en-US', { year: 'numeric', day: 'numeric', month: 'long' })}
-          </span>
-          <div>
-            <strong>
-              Width:
-            </strong>
-            <span>
-              {` ${widthRating[props.review.widthRating]}`}
-            </span>
-          </div>
-        </div>
-      </div>
-    );
-  }
-  return (
-    <div className="review">
-      <div className="leftColumn">
-        <div className="title">
+const ReviewListEntry = props => (
+  <div className="review">
+    <div className="leftColumn">
+      <div>
+        <strong>
           {props.review.title}
-        </div>
-        <div className="description">
-          {props.review.description}
-        </div>
-        <div className="reviewerInfo">
-          <span>
-            {props.review.nickname}
-          </span>
-          <span>
-            Sweepstakes entry
-          </span>
-        </div>
+        </strong>
       </div>
-      <div className="rightColumn">
+      <div className="description">
+        {props.review.description}
+      </div>
+      <div className="reviewerInfo">
         <span>
-          {new Date(props.review.date).toLocaleString('en-US', { year: 'numeric', day: 'numeric', month: 'long' })}
+          {props.review.nickname}
+        </span>
+        <span>
+          Sweepstakes entry
         </span>
       </div>
     </div>
-  );
-};
+    <div className="rightColumn">
+      <span>
+        {new Date(props.review.date).toLocaleString('en-US', { year: 'numeric', day: 'numeric', month: 'long' })}
+      </span>
+      <FitRating fit={props.review.fitRating} />
+      <WidthRating width={props.review.widthRating} />
+    </div>
+  </div>
+);
 
 export default ReviewListEntry;
