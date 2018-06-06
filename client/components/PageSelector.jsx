a5b9338305dc32e8b191797615087b492c716c2e
Jehwa Shin <s.jehwa@gmail.com>
2018-06-06 12:21:17 -0700

    added onClick event on pageSelector component

--- client/components/PageSelector.jsx
+++ client/components/PageSelector.jsx
@@ -8,97 +8,68 @@ class PageSelector extends React.Component {
       currentPage: 2,
     };
     this.pageList = this.pageList.bind(this);
+    this.pageSelect = this.pageSelect.bind(this);
   }
 
   pageList() {
     const pageList = [];
     if (this.props.totalPage < 6) {
-      for (let i = 1; i <= this.props.totalPage; i ++) {
+      for (let i = 1; i <= this.props.totalPage; i++) {
         pageList.push(i);
       }
     } else {
       if (this.state.currentPage < 4) {
-        pageList.push(1,2,3,4,'...',this.props.totalPage);
+        pageList.push(1, 2, 3, 4, '...', this.props.totalPage);
       }
-      if (this.state.currentPage > this.props.totalPage-3) {
-        pageList.push(1, '...', this.props.totalPage-3, this.props.totalPage-2, this.props.totalPage-1, this.props.totalPage);
+      if (this.state.currentPage > this.props.totalPage - 3) {
+        pageList.push(1, '...', this.props.totalPage - 3, this.props.totalPage - 2, this.props.totalPage - 1, this.props.totalPage);
       }
-      if ((this.state.currentPage >=4) && (this.state.currentPage <= this.props.totalPage-3)) {
-        pageList.push(1, '...', this.state.currentPage-1, this.state.currentPage, this.state.currentPage+1, '...', this.props.totalPage);
+      if ((this.state.currentPage >= 4) && (this.state.currentPage <= this.props.totalPage - 3)) {
+        pageList.push(1, '...', this.state.currentPage - 1, this.state.currentPage, this.state.currentPage + 1, '...', this.props.totalPage);
       }
     }
 
     return pageList;
   }
 
+  pageSelect(num) {
+    this.setState({
+      currentPage: Number(num),
+    });
+  }
+
 
   render() {
-    if (this.state.currentPage === 1) {
-      return (
-        <div className="pageSelectorBody">
-          <div style={{visibility: 'hidden'}}>
-            <svg height="24" width="13">
-              <path d="M 1 12 l 11 11 m 0 -22 L 1 12" />
-            </svg>
-          </div>         
-          <div className="selectNum">
-            {this.pageList().map((num, index) => <PageList page={num} key={index}/>)}
-          </div>
-          <div>
-            <svg height="24" width="13">
-              <path d="M 12 12 L 1 1 m 0 22 l 11 -11" />
-            </svg>
-          </div>
-        </div>
-      )
-    }
-    if(this.state.currentPage === this.props.totalPage) {
-      return (
-        <div className="pageSelectorBody">
-          <div>
-            <svg height="24" width="13">
-              <path d="M 1 12 l 11 11 m 0 -22 L 1 12" />
-            </svg>
-          </div>         
-          <div className="selectNum">
-            {this.pageList().map((num, index) => <PageList page={num} key={index}/>)}
-          </div>
-          <div style={{visibility: 'hidden'}}>
-            <svg height="24" width="13">
-              <path d="M 12 12 L 1 1 m 0 22 l 11 -11" />
-            </svg>
-          </div>
-        </div>
-      )
-    }
+    const visibility = {
+      1: 'hidden',
+    };
+    const nextVisibility = {
+      [this.props.totalPage]: 'hidden',
+    };
     return (
       <div className="pageSelectorBody">
-        <div className="arrow">
+        <div style={{ visibility: visibility[this.state.currentPage] || null }}>
           <svg height="24" width="13">
             <path d="M 1 12 l 11 11 m 0 -22 L 1 12" />
           </svg>
           <span>
             PREVIOUS
           </span>
-        </div>         
-        <div className="selectNum">
-          {this.pageList().map((num, index) => <PageList page={num} key={index}/>)}
         </div>
-        <div className="arrow">
+        <div className="selectNum">
+          {this.pageList().map((num, index) => <PageList page={num} key={index} select={this.pageSelect} />)}
+        </div>
+        <div style={{ visibility: nextVisibility[this.state.currentPage] || null }}>
           <span>
             NEXT
           </span>
-          <span>
-            <svg height="24" width="13">
-              <path d="M 12 12 L 1 1 m 0 22 l 11 -11" />
-            </svg>
-          </span>
+          <svg height="24" width="13">
+            <path d="M 12 12 L 1 1 m 0 22 l 11 -11" />
+          </svg>
         </div>
       </div>
-    )
-
+    );
   }
-
 }
 
 export default PageSelector;
