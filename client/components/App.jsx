45ddd172c322240526328e1a2e957f0a9a51fbb3
Jehwa Shin <s.jehwa@gmail.com>
2018-06-09 16:20:54 -0700

    refactored filtering and sorting functions

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -10,13 +10,13 @@ class App extends React.Component {
     super(props);
     this.state = {
       reviews: [],
-      timeline: [],
       filtered: [],
       eachPage: [],
-      sortedByStars: false,
+      filteredByStar: 0,
       sortedByTime: false,
       totalReviews: '',
       currentPage: 1,
+      itemNo: null,
     };
     this.bringData = this.bringData.bind(this);
     this.sortedByNumber = this.sortedByNumber.bind(this);
@@ -29,26 +29,25 @@ class App extends React.Component {
   }
 
   bringData() {
-    let itemNumber = window.location.pathname.split('/')[1];
+    const itemNumber = window.location.pathname.split('/')[1];
     axios.get(`/${itemNumber}/init`)
       .then((res) => {
-        let validStars = res.data.filter((review) => review.stars > 0);
-        let aveStar = validStars.reduce((acc, val) => (acc + val.stars),0)/validStars.length;
-        let validFitRatings = res.data.filter((review) => review.fitRating > 0);
-        let aveFitRating = Math.round(validFitRatings.reduce((acc, val) => (acc + val.fitRating),0)/validFitRatings.length);
-        let validWidthRatings = res.data.filter((review) => review.widthRating > 0);
-        let aveWidthRating = Math.round(validWidthRatings.reduce((acc, val) => (acc + val.widthRating),0)/validWidthRatings.length);
-        let timeline = res.data.slice().sort((a,b) => new Date(b.date) - new Date(a.date));
+        const validStars = res.data.filter(review => review.stars > 0);
+        const aveStar = validStars.reduce((acc, val) => (acc + val.stars), 0) / validStars.length;
+        const validFitRatings = res.data.filter(review => review.fitRating > 0);
+        const aveFitRating = Math.round(validFitRatings.reduce((acc, val) => (acc + val.fitRating), 0) / validFitRatings.length);
+        const validWidthRatings = res.data.filter(review => review.widthRating > 0);
+        const aveWidthRating = Math.round(validWidthRatings.reduce((acc, val) => (acc + val.widthRating), 0) / validWidthRatings.length);
 
         this.setState({
           reviews: res.data.slice(),
           filtered: res.data.slice(),
-          timeline: timeline,
-          eachPage: res.data.slice(0,5),
-          aveStar: aveStar,
-          aveFitRating: aveFitRating,
-          aveWidthRating: aveWidthRating,
+          eachPage: res.data.slice(0, 5),
+          aveStar,
+          aveFitRating,
+          aveWidthRating,
           totalReviews: res.data.length,
+          itemNo: itemNumber,
         });
       })
       .catch((err) => {
@@ -60,85 +59,96 @@ class App extends React.Component {
     let filtered;
     if(starNum === 0) {
       if(!this.state.sortedByTime) {
-        filtered = this.state.reviews;
+        filtered = this.state.reviews.slice();
       } else {
-        filtered = this.state.timeline.slice();
+        filtered = this.state.reviews.sort((a,b) => new Date(b.date) - new Date(a.date));
       }
       this.setState({
-        sortedByStars: false,
+        filtered: filtered,
+        eachPage: filtered.slice(0,5),
+        currentPage: 1,
+        filteredByStar: starNum,
       })
-    }
+    } 
     if(starNum > 0 && starNum < 6) {
-      if (!this.state.sortedByTime) {
-        filtered = this.state.reviews.filter(review => review.stars === starNum);
-      } else {
-        filtered = this.state.timeline.filter(review => review.stars === starNum);
-      }
       this.setState({
-        sortedByStars: true,
+        filteredByStar: starNum,
+      }, () => {
+        axios.get(`/${this.state.itemNo}/filter/${this.state.filteredByStar}/${this.state.sortedByTime}`)
+          .then((res) => {
+            this.setState({
+              filtered: res.data,
+              eachPage: res.data.slice(0, 5),
+              currentPage: 1,
+            });
+          });
       })
     }
-    if(starNum === 10) {
-      if(!this.state.sortedByStars) {
-        filtered = this.state.filtered.sort((a, b) => b.stars - a.stars);
-      } else {
-        filtered = this.state.filtered;
-      }
+    if(starNum === 10 && this.state.filteredByStar === 0) {
+      filtered = this.state.reviews.slice().sort((a,b) => b.stars - a.stars);
       this.setState({
+        filtered: filtered,
+        eachPage: filtered.slice(0,5),
+        currentPage: 1,
         sortedByTime: false,
       })
     }
     if(starNum === 11) {
-      if(this.state.sortedByStars) {
-        filtered = this.state.filtered.sort((a, b) => new Date(b.date) - new Date(a.date));
+      if(this.state.filteredByStar === 0) {
+        filtered = this.state.reviews.slice().sort((a,b) => new Date(b.date) - new Date(a.date));
         this.setState({
-          sortedBy: true,
+          filtered: filtered,
+          eachPage: filtered.slice(0,5),
+          currentPage: 1,
+          sortedByTime: true,
         })
       } else {
-        filtered = this.state.timeline.slice();
+        this.setState({
+          sortedByTime: true,
+        }, () => {
+          axios.get(`/${this.state.itemNo}/filter/${this.state.filteredByStar}/${this.state.sortedByTime}`)
+            .then((res) => {
+              this.setState({
+                filtered: res.data,
+                eachPage: res.data.slice(0, 5),
+                currentPage: 1,
+              });
+            });
+        })
       }
-      this.setState({
-        sortedByTime: true,
-      })
-
-    }
-    this.setState({
-      filtered: filtered,
-      eachPage: filtered.slice(0,5),
-      currentPage: 1,
-    })
+    } 
   }
 
   changePage(pageNum) {
-    let pageView = this.state.filtered.slice((5 * (pageNum - 1)), 5 * pageNum);
+    const pageView = this.state.filtered.slice((5 * (pageNum - 1)), 5 * pageNum);
     this.setState({
       eachPage: pageView,
-      currentPage: pageNum
-    })
+      currentPage: pageNum,
+    });
   }
 
   movePage(direction) {
     let newPage;
-    if(direction === 'previous') {
+    if (direction === 'previous') {
       newPage = this.state.currentPage - 1;
     } else {
       newPage = this.state.currentPage + 1;
     }
     this.setState({
       currentPage: newPage,
-      eachPage: this.state.filtered.slice((5 * (newPage - 1)), 5 * newPage)
-    })
+      eachPage: this.state.filtered.slice((5 * (newPage - 1)), 5 * newPage),
+    });
   }
 
   render() {
     return (
       <div className="nerdstromReviewPage">
         <div className="reviewSummaryMain">
-          <ReviewSummary 
-          aveStar={this.state.aveStar} 
-          aveFitRating={this.state.aveFitRating} 
-          aveWidthRating={this.state.aveWidthRating} 
-          totalReviews={this.state.totalReviews}
+          <ReviewSummary
+            aveStar={this.state.aveStar}
+            aveFitRating={this.state.aveFitRating}
+            aveWidthRating={this.state.aveWidthRating}
+            totalReviews={this.state.totalReviews}
           />
         </div>
         <div className="sorting" >
@@ -148,11 +158,11 @@ class App extends React.Component {
           <ReviewList reviews={this.state.eachPage} />
         </div>
         <div className="pageSelector">
-          <PageSelector 
-          currentPage={this.state.currentPage} 
-          totalPage={Math.ceil(this.state.filtered.length/5)} 
-          changePage={this.changePage}
-          movePage={this.movePage}
+          <PageSelector
+            currentPage={this.state.currentPage}
+            totalPage={Math.ceil(this.state.filtered.length / 5)}
+            changePage={this.changePage}
+            movePage={this.movePage}
           />
         </div>
       </div>
--- server/index.js
+++ server/index.js
@@ -14,5 +14,16 @@ app.get('/:itemNo/init', (req, res) => {
     .then(data => res.send(data));
 });
 
+app.get('/:itemNo/filter/:starNo/:sortedByTime', (req, res) => {
+  const urlSplit = req.url.split('/')
+  const itemNo = urlSplit[1];
+  const starNo = urlSplit[3];
+  const sortedByTime = urlSplit[4];
+  db.filterByStar(itemNo, starNo, sortedByTime)
+    .then(data => res.send(data))
+    .catch(err => res.statusCode(500).send(err));
+});
+
+
 app.listen(3002, () => console.log('server is listening on port 3002'));
 
