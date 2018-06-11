27807b73461da9a486c4f02f384f63f446d63542
Jehwa Shin <s.jehwa@gmail.com>
2018-06-11 12:15:25 -0700

    changed div name and built

--- client/index.js
+++ client/index.js
@@ -4,4 +4,4 @@ import ReactDOM from 'react-dom';
 import style from './main.css';
 
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById('reviews'));
